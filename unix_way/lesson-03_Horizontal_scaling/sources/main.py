82ff25f9eebd1a7528d7ec56acca9058809bca3a
denis <denis@master1.cluster.local>
2021-04-08 09:36:00 +0000

    edit several bugs

--- unix_way/lesson-03_Horizontal_scaling/sources/main.py
+++ unix_way/lesson-03_Horizontal_scaling/sources/main.py
@@ -17,5 +17,5 @@ def hello():
 
     return jsonify(response)
 
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = '8080')
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port='8080')
